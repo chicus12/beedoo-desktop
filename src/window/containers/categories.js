1a34446901e024825aa7e44e1390d80c1abc86ef
chicus12 <chicus12@gmail.com>
2017-06-30 18:12:56 -0600

    feat(login): Login design fixed

--- src/window/containers/categories.js
+++ src/window/containers/categories.js
@@ -15,30 +15,55 @@ import logo from '../assets/beedoo.png'
 
 require('../styles/main.sass')
 
-
 class Categories extends React.Component {
   render() {
     console.log('logo', logo)
     return (
       <section className="sign">
-        <div className="app-name">
-          <h2>BeeDoo</h2>
-        </div>
-        <div className="line">
-          <span />
-        </div>
         <div className="logo">
           <img src={logo} alt="beedo" />
         </div>
-        <Form>
-          <input type="text" placeholder="token" />
+        <div className="app-name">
+          <h2>BeeDoo</h2>
+        </div>
+        <Form className="login-form">
+          <div className="input-container">
+            <input type="text" id="email" required />
+            <label htmlFor="email"><i className="icon-envelop" /> Email</label>
+            <div className="bar" />
+          </div>
+
+          <div className="input-container">
+            <input type="password" id="password" required />
+            <label htmlFor="password"><i className="icon-key" /> Password</label>
+            <div className="bar" />
+          </div>
+
           <div className="submit">
             <input type="submit" value="Sign In" />
           </div>
         </Form>
+
+        <span className="not-member">
+          Not a member yet? <a href="#">SignUp Now</a>
+        </span>
+
+        <span className="alternative-login">
+          Or Login with <i className="icon-ctrl icon-rotate"></i>
+        </span>
+
+        <div className="auth">
+          <div className="buttons facebook">
+            <i className="icon-facebook"></i> <span>Login with Facebook</span>
+          </div>
+          <div className="buttons google">
+            <i className="icon-google-plus"></i> <span>Login with Google</span>
+          </div>
+        </div>
+
         <footer>
           <span className="contributors">
-            made with <i className="icon-heart" /> by @chicus12, @foxdie, @eslid
+            made with <i className="icon-heart" /> by chicus12, foxdie, eslid
           </span>
         </footer>
       </section>
